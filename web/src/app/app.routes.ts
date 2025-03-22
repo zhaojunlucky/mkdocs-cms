9b43cc0ebcf3f01262c57a1f464ca8bc0e19d609
magicworldz <1940295+zhaojunlucky@users.noreply.github.com>
2025-03-22 17:31:07 +0800

    clean up

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RepositoryDetailComponent } from './repositories/repository-detail/repository-detail.component';
-import { RepositoryFormComponent } from './repositories/repository-form/repository-form.component';
 import { RepositoryImportComponent } from './repositories/repository-import/repository-import.component';
 import { EditRepositoryComponent } from './repositories/edit-repository/edit-repository.component';
 import { authGuard } from './auth/auth.guard';
@@ -11,7 +10,6 @@ export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-  { path: 'repositories/new', component: RepositoryFormComponent, canActivate: [authGuard] },
   { path: 'repositories/import', component: RepositoryImportComponent, canActivate: [authGuard] },
   { path: 'repositories/:id', component: RepositoryDetailComponent, canActivate: [authGuard] },
   { path: 'repositories/:id/edit', component: EditRepositoryComponent, canActivate: [authGuard] },
